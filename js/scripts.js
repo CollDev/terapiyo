e9416d57c312aff59177b8c09c8cb0078bee2840
Joe Robles <joe.robles.pdj@gmail.com>
2013-11-13 18:26:03 -0500

    modals added

--- js/scripts.js
+++ js/scripts.js
@@ -2,20 +2,37 @@
     Created on : 24/10/2013, 12:54:09 PM
     Author     : Joe Robles <joe.robles.pdj@gmail.com>
 */
-var $modal = '<div class="modal-dialog">\n\
-    <div class="modal-content">\n\
-        <div class="modal-body">\n\
-            <p>{{ body }}</p>\n\
-        </div>\n\
-    </div>\n\
-</div>';
-
 $('span.ca-icon').on('click', function(e){
     e.preventDefault();
     var $this = this;
-    var data = {
-        body: '<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.</p>',
-    };
-    var modal = $.mustache($modal, data);
-    $('#terapiyoModal').html(modal).modal();
+    $.get("/js/mustachejs-templates/" + $($this).attr('class').substr(8) + ".json", function(data){
+        $.get("/js/mustachejs-templates/modal.html", function(modal){
+            var $modal = $.mustache(modal, data);
+            $('#terapiyoModal').html($modal).modal();
+        });
+    });
+});
+$('span.cat-icon').on('click', function(e){
+    e.preventDefault();
+    $.get("/js/mustachejs-templates/terapiyo.json", function(data){
+        $.get("/js/mustachejs-templates/terapiyo-modal.html", function(modal){
+            var $modal = $.mustache(modal, data);
+            $('#terapiyoModal').html($modal).modal();
+        });
+    });
+});
+$('.contacto').on('click', function(e){
+    e.preventDefault();
+        $.get("/js/mustachejs-templates/contacto-modal.html", function(modal){
+            $('#terapiyoModal').html(modal).modal();
+        });
+});
+$('.noticias').on('click', function(e){
+    e.preventDefault();
+    $.get("/js/mustachejs-templates/noticias.json", function(data){
+        $.get("/js/mustachejs-templates/noticias-modal.html", function(modal){
+            var $modal = $.mustache(modal, data);
+            $('#terapiyoModal').html($modal).modal();
+        });
+    });
 });

dcf92dc1621bf7f0bb2514cbf693f9bfb15f8bf8
Joe Robles <joe@colldev.com>
2013-11-16 22:43:48 -0500

    contact form finished

--- includes/json.php
+++ includes/json.php
@@ -5,89 +5,84 @@ if (!empty($_GET)) {
         echo (require_once $_GET['course'] . '.json');
     }
 } elseif (!empty($_POST)) {
-    if (isset($_POST['nombre']) && isset($_POST['email']) && isset($_POST['comentario'])) {
+    if (isset($_POST['nombre']) && isset($_POST['email']) && isset($_POST['consulta'])) {
         $nombre = $_POST['nombre'];
         $email = $_POST['email'];
-        $comentario = $_POST['comentario'];
+        $consulta = $_POST['consulta'];
 
-        if ($nombre != '' && $email != '' && $comentario != '') {
+        if ($nombre != '' && $email != '' && $consulta != '') {
             $nameRegex    = '/^[a-zA-Z]+(([\'\,\.\- ][a-zA-Z ])?[a-zA-Z]*)*$/';
             $phoneRegex   = '/[0-9-()+]{3,20}/';
             $emailRegex   = '/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/';
-            if (preg_match($nameRegex, $nombre) == 0)
+            if (preg_match($nameRegex, $nombre) == 0) {
                 $return = array('responseCode' => 400, 'response' => 'Ha ingresado un nombre no válido.');
-            elseif (preg_match($emailRegex, $email) == 0)
+            } elseif (preg_match($emailRegex, $email) == 0) {
                 $return = array('responseCode' => 400, 'response' => 'Ha ingresado un email no válido.');
-            elseif (isset($_POST['telefono']) && $_POST['telefono'] != '' && preg_match($phoneRegex, $_POST['telefono']) == 0)
+            } elseif (isset($_POST['telefono']) && $_POST['telefono'] != '' && preg_match($phoneRegex, $_POST['telefono']) == 0) {
                 $return = array('responseCode' => 400, 'response' => 'Ha ingresado un teléfono no válido.');
-            else {
-                $apellidos = isset($_POST['apellidos']) ? "'" . $_POST['apellidos'] . "'" : NULL;
+            } else {
                 $telefono  = isset($_POST['telefono']) ? "'" . $_POST['telefono'] . "'" : NULL;
-                $direccion = isset($_POST['direccion']) ? "'" . $_POST['direccion'] . "'" : NULL;
                 //enviar el correo
                 require_once 'eml.php';
                 $message = Swift_Message::newInstance()
                     ->setSubject('Mensaje desde la página web')
                     ->setFrom($email)
                     ->setTo($email)
-                    //->setBcc($emlusername)
+                    ->setBcc($emlusername)
                     ->setBody('Hola:' . '
 ' . '
 Nombre: ' . $nombre . '
-Apellidos: ' . $apellidos . '
 Telefono: ' . $telefono . '
-Dirección: ' . $direccion . '
 Email: ' . $email . '
-Escribió el siguiente comentario:' . '
-' . $comentario . '
+Escribió la siguiente consulta:' . '
+' . $consulta . '
 ' . '
-Que tenga un buen dia.')
-                    ;
+Que tenga un buen dia.');
+
                 $sent = $mailer->send($message);
                 //guardar en la base de datos
                 require_once 'db.php';
                 $query = "INSERT INTO `correos` (
                             `id`,
                             `nombre`,
-                            `apellidos`,
                             `telefono`,
-                            `direccion`,
                             `email`,
-                            `comentario`,
+                            `consulta`,
                             `creado`,
                             `estado`
                           ) VALUES (
                             NULL,
                             '". $nombre . "',
-                            " . $apellidos . ",
                             " . $telefono . ",
-                            " . $direccion . ",
                             '". $email . "',
-                            '". $comentario . "',
+                            '". $consulta . "',
                             NOW(),
                             '". $sent . "'
                           );";
                 $inserted = mysql_query($query);
-                if ($sent && $inserted)
-                    $return = array('responseCode' => 200, 'response' => 'Comentario enviado y almacenado.');
-                else if ($sent)
-                    $return = array('responseCode' => 200, 'response' => 'Comentario enviado.');
-                else if ($inserted)
-                    $return = array('responseCode' => 200, 'response' => 'Comentario almacenado.');
-                else
+                if ($sent && $inserted) {
+                    $return = array('responseCode' => 200, 'response' => 'Consulta enviada y almacenada.');
+                } else if ($sent) {
+                    $return = array('responseCode' => 200, 'response' => 'Consulta enviada.');
+                } else if ($inserted) {
+                    $return = array('responseCode' => 200, 'response' => 'Consulta almacenada.');
+                } else {
                     $return = array('responseCode' => 400, 'response' => 'No se pudo enviar, intente más tarde.');
+                }
             }
         } else {
-            if ($nombre == '')
+            if ($nombre == '') {
                 $return = array('responseCode' => 400, 'response' => 'Debe ingresar su nombre.');
-            elseif ($email == '')
+            } elseif ($email == '') {
                 $return = array('responseCode' => 400, 'response' => 'Debe ingresar su correo electrónico.');
-            elseif ($comentario == '')
-                $return = array('responseCode' => 400, 'response' => 'Debe ingresar un comentario.');
+            } elseif ($consulta == '') {
+                $return = array('responseCode' => 400, 'response' => 'Debe ingresar una consulta.');
+            }
         }
     } else {
         $return = array('responseCode' => 400, 'response' => 'Debe usar el formulario de contácto de la página web.');
     }
+    header("Content-Type: application/json; charset=utf-8");
     echo json_encode($return);
 } else {
     echo 'No esta permitido a esta sección.';
--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,11 @@
     Created on : 24/10/2013, 12:54:09 PM
     Author     : Joe Robles <joe.robles.pdj@gmail.com>
 */
+// preload the arrow //
+if(document.images) {
+  arrow = new Image(7,80); 
+  arrow.src = "../images/msg_arrow.gif"; 
+}
 $('span.adul').on('click', function(e){
     e.preventDefault();
     var $this = this;
@@ -37,26 +42,76 @@ $('.noticias').on('click', function(e){
         });
     });
 });
-var myCenter=new google.maps.LatLng(53, -1.33);
-var marker=new google.maps.Marker({
-    position:myCenter
-});
+$(document).on('click', ".btn-secondary", function(e) {
+    e.preventDefault();
+    var nombre       = $("form#contact input#nombre"),
+        email        = $("form#contact input#email"),
+        telefono     = $("form#contact input#telefono"),
+        comentario   = $("form#contact #consulta"),
+        nameRegex    = /^[a-zA-Z]+(([\'\,\.\- ][a-zA-Z ])?[a-zA-Z]*)*$/,
+        phoneRegex   = /[0-9-()+]{3,20}/,
+        emailRegex   = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
-function initialize() {
-    var mapProp = {
-        center:myCenter,
-        zoom: 14,
-        draggable: false,
-        scrollwheel: false,
-        mapTypeId:google.maps.MapTypeId.ROADMAP,
-    };
-
-    var map=new google.maps.Map(document.getElementById("map-canvas"),mapProp);
-    marker.setMap(map);
-
-    google.maps.event.addListener(marker, 'click', function() {
-        infowindow.setContent(contentString);
-        infowindow.open(map, marker);
+    if (nombre.val() == "") {
+        inlineMsg('nombre','Debe ingresar su nombre.',3);
+        nombre.focus();
+        return false;
+    } else if(!nombre.val().match(nameRegex)) {
+        inlineMsg('nombre','Ha ingresado un nombre no válido.',3);
+        nombre.focus();
+        return false;
+    } else if (telefono.val() != '' && !telefono.val().match(phoneRegex)) {
+        inlineMsg('telefono','Ha ingresado un teléfono no válido.',3);
+        telefono.focus();
+        return false;
+    } else if (email.val() == "") {
+        inlineMsg('email','Debe ingresar su correo electrónico.',3);
+        email.focus();
+        return false;
+    } else if (!email.val().match(emailRegex)) {
+        inlineMsg('email','Ha ingresado un correo no válido.',3);
+        email.focus();
+        return false;
+    } else if (comentario.val() == "") {
+        inlineMsg('consulta','Debe ingresar un comentario.',3);
+        comentario.focus();
+        return false;
+    }
+    $form = $('form#contact');
+    $.ajax({
+        type: "POST",
+        url: $form.attr("action"),
+        data: $form.serialize(),
+        dataType: "json"
+    }).done(function(data){
+        var type = 'danger';
+        var title = 'Error';
+        var top = false;
+        if (data.responseCode == 200) {
+            $('#flash_message').remove();
+            type = 'success';
+            title = 'Éxito';
+            top = true;
+            $form.each(function(){
+                this.reset();
+            });
+            $('a.volver').trigger('click');
+        }
+        $('#flash_message').html('<strong id="flash_title"></strong>&nbsp;&nbsp;').append(data.response).message(type, title, '#flash_title', top);
     });
-
-};
+});
+(function($){
+    $.fn.extend({
+        message: function(type, title, title_id, top){
+            $(title_id).html('').append(title);
+            var del = 0;
+            if ($(window).scrollTop() !== 0) {
+                if (top) {
+                    $("html, body").animate({ scrollTop: 0 }, 600);
+                }
+                del = 600;
+            }
+            $(this).removeClass().addClass('alert').addClass('alert-' + type).delay(del).slideDown().delay(3000).slideUp();
+        }
+    });
+})(jQuery);
